60b9e138ad5010d8c5ff0407b0ea4715e48791e1
Alexia Cooze <alexiacooze@Alexias-MacBook-Air.local>
2022-06-30 17:01:19 -0400

    created functionality for correct and incorrect for each individual card, need to add a seperate button for clear

--- src/components/Hooks/Hooks.js
+++ src/components/Hooks/Hooks.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { CATEGORIES_API } from "../../utils/appConfig";
-import "../Hooks/Hooks.scss"
+import "../Hooks/Hooks.scss";
 import back from "../../assets/Images/back.png";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 export default function Hooks() {
-  
+  // creating state for the flashcard toggle and calling the API
+
   const [flashcards, setFlipCard] = useState([]);
 
   useEffect(() => {
     CATEGORIES_API.getAll("hooks").then((res) => setFlipCard(res.data));
     // console.log(res);
   }, []);
-  
 
   const flip = (id) => {
     const filterId = flashcards.filter((item) => item.id === id);
@@ -29,20 +29,24 @@ export default function Hooks() {
     setFlipCard(filterFlipped);
   };
 
+  const answer = (id, correct) => {
+    // filtering the id from flashcard data and storing it within filterId
+    const filterId = flashcards.filter((item) => item.id === id);
 
+    // creating new object that takes filter id and stores it within the new object, sets the correct param to correct
+    const updatedFilterId = {
+      ...filterId[0],
+      correct: correct,
+    };
 
-  const [colorRed, setColorRed] = useState(false);
+    // filtering the id and pushing the new filterFlipped into the updatedFilterId above
+    const filterFlipped = flashcards.filter((item) => item.id !== id);
 
-  const handleClick = e => {
-    setColorRed(current => !current);
+    filterFlipped.push(updatedFilterId);
+    // sorting the filterFlipped and then setting the new state to the setFlipCard
+    filterFlipped.sort((a, b) => (a.id > b.id ? 1 : -1));
+    setFlipCard(filterFlipped);
   };
-
-  const [colorGreen, setColorGreen] = useState(false);
-
-  const handleChange = e => {
-    setColorGreen(current => !current);
-  };
-
 
   return (
     <section className="hooks">
@@ -55,25 +59,54 @@ export default function Hooks() {
       <div className="hooks__card-divider">
         {flashcards.map((card) => {
           return (
-            <div
-              className={`hooks__card-container ${
-                card.flipped ? "hooks__flip-card" : ""
-              } `}
-            >
+            <>
               <div
-                onClick={() => flip(card.id)}
-                
-                className={`hooks__card ${colorRed ? 'hooks__incorrect-background' : ''} ${colorGreen ? 'hooks__correct-background' : ''}`}
-                key={card.id}
+                className={`hooks__card-container ${
+                  card.flipped ? "hooks__flip-card" : ""
+                } `}
               >
-                <p>{card.flipped ? card.answer : card.questions}</p>
-                
-              <div className="hooks__select-container">
-                <p  className="hooks__correct" onClick={handleChange}>Correct</p>
-                <p className="hooks__incorrect" onClick={handleClick}>Incorrect</p>
+                <div
+                  onClick={() => flip(card.id)}
+                  // check cards.correct if it is true then evaluate if cards === correct, the value set within the onClick, if it is true then set the background color to the class of correct-background, if it is false then set the background color to class incorrect-background
+                  className={`hooks__card 
+                  
+                  
+                  ${
+                    card.correct
+                      ? card.correct === "correct"
+                        ? "hooks__correct-background"
+                        : "hooks__incorrect-background"
+                      : ""
+                  }
+
+                  
+                  `}
+                  key={card.id}
+                >
+                  <p>{card.flipped ? card.answer : card.questions}</p>
+                </div>
               </div>
-              </div>
-            </div>
+              {card.flipped ? (
+                <div className="hooks__select-container">
+                  <p
+                    className="hooks__correct"
+                    // correct is evaluated as a string within the ternary card.correct === "correct"
+                    onClick={() => answer(card.id, "correct")}
+                  >
+                    Correct
+                  </p>
+                  <p
+                    className="hooks__incorrect"
+                    // incorrect is not evaluated as correct is true, therefore "incorrect" is just a place holder. The placeholder only needs a truthy value in order for the ternary to work
+                    onClick={() => answer(card.id, "incorrect")}
+                  >
+                    Incorrect
+                  </p>
+                </div>
+              ) : (
+                ""
+              )}
+            </>
           );
         })}
       </div>
